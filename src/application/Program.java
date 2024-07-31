dabf88598915f31418f6e83a5b16859f0f94e6f8
Caua Ananias <caua.ananias2004@gmail.com>
2024-07-31 16:03:08 -0400

    Implementado limpeza de terminal e exceções

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,30 +1,35 @@
-package application;
+    package application;
 
-import boardgame.Board;
-import chess.ChessMatch;
-import chess.ChessPiece;
-import chess.ChessPosition;
+    import chess.ChessException;
+    import chess.ChessMatch;
+    import chess.ChessPiece;
+    import chess.ChessPosition;
 
-import java.util.Scanner;
+    import java.util.InputMismatchException;
+    import java.util.Scanner;
 
-public class Program {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ChessMatch chessMatch = new ChessMatch();
+    public class Program {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            ChessMatch chessMatch = new ChessMatch();
 
+            while (true) {
+                try {
+                    UI.clearScreen();
+                    UI.printBoard(chessMatch.getPieces());
+                    System.out.println();
+                    System.out.print("Source: ");
+                    ChessPosition source = UI.readChessPosition(sc);
 
-        while (true) {
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
+                    System.out.println();
+                    System.out.print("Target: ");
+                    ChessPosition target = UI.readChessPosition(sc);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
-
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-
-        }
+                    ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                } catch (ChessException | InputMismatchException e){
+                    System.out.println(e.getMessage());
+                    sc.nextLine();
+                }
+            }
     }
 }

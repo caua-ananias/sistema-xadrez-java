a19b5aba26d2006741d46919b37fb59cd337cf4e
Caua Ananias <caua.ananias2004@gmail.com>
2024-07-31 16:27:03 -0400

    Implementando os possiveis movimentos de uma peça

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
     protected Position position;
     private Board board;
 
@@ -11,5 +11,23 @@ public class Piece {
 
     protected Board getBoard() {
         return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibleMove(Position pos) {
+        return possibleMoves()[pos.getRow()][pos.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove() {
+        boolean[][] mat = possibleMoves();
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
